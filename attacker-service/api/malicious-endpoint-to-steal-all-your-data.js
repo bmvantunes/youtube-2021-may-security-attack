339516367928323b4d24345497e43ef9db2d8123
Bruno Antunes <bmvantunes@users.noreply.github.com>
2021-06-03 20:26:39 +0100

    Update malicious-endpoint-to-steal-all-your-data.js

--- attacker-service/api/malicious-endpoint-to-steal-all-your-data.js
+++ attacker-service/api/malicious-endpoint-to-steal-all-your-data.js
@@ -1,4 +1,6 @@
 module.exports = (req, res) => {
+  // this line might be a trigger to some security tools - it's just here to avoid myself creating yet another repository LOL
+  // in a real attack, the attacker would not have the API endpoint close to the code for all of us to see!!! =D
   res.setHeader("Access-Control-Allow-Origin", "*");
 
   console.log(`${req.method}: malicious-endpoint received:`, req.body);
